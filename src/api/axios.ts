32af8f9af7c647c838d5480b940ecc7277624615
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-11-17 13:03:13 +0500

    [Fix] link issue

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "1836eb21d80b44f281d70421241711";
-const BASE_URL = "http://api.weatherapi.com/v1";
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
